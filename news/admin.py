eafdbc6e88f5db6684563bb13b5e4a210e122490
roz-pnv <rozhinpanaw@gmail.com>
2025-07-15 05:27:38 +0330

    fix(admin): enable multi-tag selection for News model

--- news/admin.py
+++ news/admin.py
@@ -4,6 +4,10 @@ from .models.news import News
 from .models.news_image import NewsImage
 from .models.tag import Tag
 
-admin.site.register(News)
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'published_at']
+    filter_horizontal = ['tags']
+    
 admin.site.register(NewsImage)
 admin.site.register(Tag)
