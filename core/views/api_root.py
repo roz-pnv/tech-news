d20618adaa46e99c6df9a310c6c2868357ee0702
roz-pnv <rozhinpanaw@gmail.com>
2025-07-16 14:04:32 +0330

    chore(api): allow public access to API root view with AllowAny permission

--- core/views/api_root.py
+++ core/views/api_root.py
@@ -1,10 +1,13 @@
 from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 from core.constants.api_endpoints import API_ENDPOINTS
 
 class APIRootView(APIView):
+    permission_classes = [AllowAny]
+    
     def get(self, request, format=None):
         return Response({
             "endpoints": API_ENDPOINTS,
